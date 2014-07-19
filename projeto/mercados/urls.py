167a876864eb19180b0f2185f1d99ee4974eea7c
Paulo Victor <pvictorpessoa@hotmail.com>
2014-07-19 15:47:52 -0300

    alterando nome do projeto

--- projeto/mercados/urls.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('projeto.mercados.views',
-    url(r'^anuncio/$', 'anuncio_create', name='anuncio_create'),
-    )
